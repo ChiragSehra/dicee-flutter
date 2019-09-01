c625dd8bb3ffa7ce529fb8f8497e7db10a1565f7
ChiragSehra <chiragsehra42@gmail.com>
2019-09-01 18:16:49 +0530

    randomizing the dice faces number

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,11 +16,19 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void changeDiceFace() {}
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 2;
-    var rightDiceNumber = 4;
     return Center(
       child: Row(children: <Widget>[
         Expanded(
@@ -27,7 +36,11 @@ class DicePage extends StatelessWidget {
             child: Image.asset('images/dice$leftDiceNumber.png'),
             onPressed: () {
               //void callback
-              print("Left button pressed");
+              setState(() {
+                leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
+                print('diceNumber=$leftDiceNumber');
+              });
             },
           ),
         ),
@@ -35,7 +48,11 @@ class DicePage extends StatelessWidget {
           child: FlatButton(
             child: Image.asset('images/dice$rightDiceNumber.png'),
             onPressed: () {
-              print("Right button pressed");
+              setState(() {
+                rightDiceNumber = Random().nextInt(6) + 1;
+                leftDiceNumber = Random().nextInt(6) + 1;
+                print('diceNumber=$rightDiceNumber');
+              });
             },
           ),
         ),
